337bf8e5711187d31173634f7e733628c0df5f16
Atsushi-Takayama <149247777+Atsushi-Takayama@users.noreply.github.com>
2023-12-06 23:49:06 +0900

    Add files via upload

---
+++ kadai_004/Score_Chapter04.java
@@ -0,0 +1,20 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+      System.out.println("Aさんの点数は１０点");
+      System.out.println("Bさんの点数は2０点");
+      System.out.println("Cさんの点数は3０点");
+      System.out.println("Dさんの点数は4０点");
+      System.out.println("Eさんの点数は5０点");
+      System.out.println("Fさんの点数は6０点");
+      System.out.println("Gさんの点数は7０点");
+      System.out.println("Hさんの点数は8０点");
+      System.out.println("Iさんの点数は9０点");
+      System.out.println("Jさんの点数は100点");
+      System.out.println("平均点は" + (10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90 + 100)/10 + "点");
+	}
+
+}
