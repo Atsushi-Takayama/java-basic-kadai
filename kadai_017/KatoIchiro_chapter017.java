ff141b7ef324e78575fb7845f999cf4559bd972d
Atsushi-Takayama <149247777+Atsushi-Takayama@users.noreply.github.com>
2023-12-13 00:27:16 +0900

    Add files via upload

---
+++ kadai_017/KatoIchiro_chapter017.java
@@ -0,0 +1,10 @@
+package kadai_017;
+
+public class KatoIchiro_chapter017 extends Kato_chapter017 {
+	public void setGivenName(String name) {
+		 this.givenName = "一郎";
+	}
+	 public void eachIntroduce() {
+		System.out.println("好きな食べ物はリンゴです");
+	}
+}
---
+++ kadai_017/Kato_chapter017.java
@@ -0,0 +1,17 @@
+package kadai_017;
+
+abstract class Kato_chapter017 {
+	public String familyName = "加藤";
+    public String givenName;
+    public String address = "東京都中野区〇×";
+    
+    public void commonIntroduce() {
+   	  System.out.println("名前は" + this.familyName + this.givenName + "です");
+   	  System.out.println("住所は" + this.address + "です" );
+    }
+    abstract public void eachIntroduce();
+    
+    public void execIntroduce() {
+   	 
+    }
+}
