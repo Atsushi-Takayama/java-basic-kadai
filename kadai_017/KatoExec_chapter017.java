3ecae6ecf9ed899991a47a2a595aa12137dcb209
Atsushi-Takayama <149247777+Atsushi-Takayama@users.noreply.github.com>
2023-12-13 22:16:38 +0900

    Add files via upload

---
+++ kadai_017/KatoExec_chapter017.java
@@ -0,0 +1,26 @@
+package kadai_017;
+
+public class KatoExec_chapter017 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+      KatoTaro_chapter017 taro = new KatoTaro_chapter017();
+      KatoIchiro_chapter017 ichiro = new KatoIchiro_chapter017();
+      KatoHanako_chapter017 hanako = new KatoHanako_chapter017();
+      
+      
+      hanako.setGivenName();
+      ichiro.setGivenName();
+      taro.setGivenName();
+      
+      taro.commonIntroduce();
+      taro.eachIntroduce();
+      ichiro.commonIntroduce();
+      ichiro.eachIntroduce();
+      hanako.commonIntroduce();      
+      hanako.eachIntroduce();
+      
+      
+	}
+
+}
