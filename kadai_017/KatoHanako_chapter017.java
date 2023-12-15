d5b386b83dba2bc6eb7340ae32576c146cfcdc34
Atsushi-Takayama <149247777+Atsushi-Takayama@users.noreply.github.com>
2023-12-15 23:51:38 +0900

    Add files via upload

---
+++ kadai_017/KatoHanako_chapter017.java
@@ -0,0 +1,10 @@
+package kadai_017;
+
+public class KatoHanako_chapter017 extends Kato_chapter017{
+   public void setGivenName() {
+	 this.givenName = "花子";
+   }
+   public void eachIntroduce() {
+	   System.out.println("趣味は読書です");
+   }
+}
