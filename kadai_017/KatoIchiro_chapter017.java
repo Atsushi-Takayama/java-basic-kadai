282eab1ea55bd1352c7d0b7870f4c01367305762
Atsushi-Takayama <149247777+Atsushi-Takayama@users.noreply.github.com>
2023-12-16 17:51:51 +0900

    Add files via upload

---
+++ kadai_017/KatoIchiro_chapter017.java
@@ -0,0 +1,11 @@
+package kadai_017;
+
+public class KatoIchiro_chapter017 extends Kato_chapter017 {
+	public void setGivenName() {
+		 this.givenName = "一郎";
+		
+	}
+	 public void eachIntroduce() {
+		System.out.println("好きな食べ物はリンゴです");
+	}
+}
