60fd2f10fa5060e1404253d78b03112172565df6
Atsushi-Takayama <149247777+Atsushi-Takayama@users.noreply.github.com>
2024-02-07 23:34:52 +0900

    Delete samuraitravel directory

--- samuraitravel/src/main/java/com/example/samuraitravel/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.samuraitravel.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.samuraitravel.entity.User;
-public interface UserRepository extends JpaRepository<User,Integer>{
-	public User findByEmail(String email);
-}
