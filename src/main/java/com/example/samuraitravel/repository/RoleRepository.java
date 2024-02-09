bd2f86cfe97feef01ef6571776876ef09c45c934
Atsushi-Takayama <149247777+Atsushi-Takayama@users.noreply.github.com>
2024-02-09 15:23:37 +0900

    Add files via upload

---
+++ src/main/java/com/example/samuraitravel/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.example.samuraitravel.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.samuraitravel.entity.Role;
+public interface RoleRepository extends JpaRepository<Role,Integer>{
+   public Role findByName(String name);
+}
