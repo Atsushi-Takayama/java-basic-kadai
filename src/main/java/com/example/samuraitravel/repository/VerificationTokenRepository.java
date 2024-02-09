28cfe004800f07939927cb099c589fde9f5a503e
Atsushi-Takayama <149247777+Atsushi-Takayama@users.noreply.github.com>
2024-02-09 15:24:04 +0900

    Delete src directory

--- src/main/java/com/example/samuraitravel/repository/VerificationTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.samuraitravel.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.samuraitravel.entity.VerificationToken;
-
-public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Integer>{
-   public VerificationToken findByToken(String token);
-}
