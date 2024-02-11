0cc606edcf1b104b318edc6d233b7b0887d9ea36
Atsushi-Takayama <149247777+Atsushi-Takayama@users.noreply.github.com>
2024-02-11 23:05:58 +0900

    Delete src directory

--- src/Procfile
+++
@@ -1,1 +0,0 @@
-web: java -Dserver.port=$PORT $JAVA_OPTS -jar -Dspring.profiles.active=production target/*.jar
--- src/target/classes/static/js/flatpickr.js
+++
@@ -1,9 +0,0 @@
-let maxDate = new Date();
-maxDate = maxDate.setMonth(maxDate.getMonth() + 3);
-
-flatpickr('#fromCheckinDateToCheckoutDate', {
-  mode: "range",
-  locale: 'ja',
-  minDate: 'today',
-  maxDate: maxDate
-});
