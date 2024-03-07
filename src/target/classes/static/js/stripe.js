f3101b525917b32d3708735a4a573ba3351562c8
Atsushi-Takayama <149247777+Atsushi-Takayama@users.noreply.github.com>
2024-03-08 00:08:13 +0900

    Delete src directory

--- src/target/classes/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51OdYf6G6lnlAf5IPoT6FTDYhTSI1ObMODIiP3LTRDAzunS5vaK6Ilw0v7H8G1aYwhQyCINt00Dle3YkFlhy2mmZa00VgOE4Je2');
-const paymentButton = document.querySelector('#paymentButton');
-
-paymentButton.addEventListener('click', () => {
-  stripe.redirectToCheckout({
-    sessionId: sessionId
-  })
-});
